fbdc95f272076285d730bb3999b6f1dfb37d3a59
Hillary Kithinji <hillary.kithinjihilary@gmail.com>
2026-02-15 19:13:49 +0300

    Initial clean commit: add notes-management-api folder only

---
+++ notes-management-api/notes/views.py
@@ -0,0 +1,15 @@
+from rest_framework import viewsets, permissions
+from .models import Note
+from .serializers import NoteSerializer
+
+class NoteViewSet(viewsets.ModelViewSet):
+    serializer_class = NoteSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        # Only return notes for the logged-in user
+        return Note.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        # Automatically assign the logged-in user
+        serializer.save(user=self.request.user)
