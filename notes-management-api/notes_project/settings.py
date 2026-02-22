bf956fdff5c1f0d0065682c77aed2f24542b1e4c
Hillary Kithinji <hillary.kithinjihilary@gmail.com>
2026-02-22 14:34:40 +0300

    Clean up tests and finalize project settings

--- notes-management-api/notes_project/settings.py
+++ notes-management-api/notes_project/settings.py
@@ -37,9 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'notes',
-]
 
+    'rest_framework',
+    'rest_framework.authtoken',
+
+    'notes',
+    'drf_yasg',
+]
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -50,6 +54,14 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
 ROOT_URLCONF = 'notes_project.urls'
 
 TEMPLATES = [
