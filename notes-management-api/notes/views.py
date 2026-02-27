eec8fc9750d16ac3e871a1b5c09f99e0d79e2606
Hillary Kithinji <hillary.kithinjihilary@gmail.com>
2026-02-27 11:30:16 +0300

    Update Register and Login views to allow public access

--- notes-management-api/notes/views.py
+++ notes-management-api/notes/views.py
@@ -5,20 +5,27 @@ from rest_framework.views import APIView
 from django.contrib.auth import authenticate
 from .serializers import NoteSerializer, UserSerializer
 from .models import Note
+from rest_framework.permissions import AllowAny
 
 # Register view
 class RegisterView(generics.CreateAPIView):
     serializer_class = UserSerializer
+    permission_classes = [AllowAny]   
 
 # Login view
 class LoginView(APIView):
+    permission_classes = [AllowAny]  
+
     def post(self, request):
         username = request.data.get('username')
         password = request.data.get('password')
+
         user = authenticate(username=username, password=password)
+
         if user:
             token, created = Token.objects.get_or_create(user=user)
             return Response({'token': token.key})
+
         return Response({'error': 'Invalid credentials'}, status=400)
 
 # Notes list and create
